a8e4d62c8115d713b76d1ba79719c873fdca9713
unknown <achonwaechris@outlook.com>
2022-02-26 05:03:11 +0100

    finish color challenge

--- Qazi_JS_Course/JS_on_staroids/app.js
+++ Qazi_JS_Course/JS_on_staroids/app.js
@@ -99,3 +99,55 @@ function rpsFrontEnd(humanSelect, botSelect, finalMassage) {
     document.getElementById('flex-box-rps-div').appendChild(massageDiv);
     document.getElementById('flex-box-rps-div').appendChild(botDiv);
 }   
+
+var all_buttons = document.getElementsByTagName('button');
+console.log(all_buttons);
+
+var copyAllButton = [];
+for  ( let i=0; i < all_buttons.length; i++) {
+    copyAllButton.push(all_buttons[i]);
+}
+
+console.log(copyAllButton);
+
+function buttonColorChange(buttonThingy) {
+    if (buttonThingy.value === 'red') {
+        buttonRed();
+    } else if (buttonThingy.value === 'green'){
+        buttonGreen();
+    } else if (buttonThingy.value === 'reset'){
+        buttonReset();
+    } else if (buttonThingy.value === 'random') {
+        buttonRandom();
+    }
+}
+
+function buttonRed () {
+    for (let i=0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
+    }
+}
+
+function buttonGreen () {
+    for (let i=0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+}
+function buttonReset () {
+    for (let i=0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButton[i]);
+    }
+}
+
+function buttonRandom () {
+   let choices = ['btn-primary', 'btn-danger', 'btn-success', 'btn-warning']
+
+   for (let i=0; i < all_buttons.length; i++){
+       let randomNumber = Math.floor(Math.random() * 4);
+       all_buttons[i].classList.remove(all_buttons[i].classList());
+       all_buttons[i].classList.add(choices[randomNumber]);
+   }
+}
